fb613869112f8c8761ad90eb95875b7b6a456134
Arhizmei <arhizmei@gmail.com>
2023-05-28 14:32:30 +0300

    optimisationCode

--- app/src/main/java/com/zmei/gdzs/MainActivity2.kt
+++ app/src/main/java/com/zmei/gdzs/MainActivity2.kt
@@ -14,11 +14,13 @@ import drawable.ItemOffsetDecoration
 import java.text.SimpleDateFormat
 import java.util.*
 
-class MainActivity2 : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+class MainActivity2 : AppCompatActivity(), AdapterView.OnItemSelectedListener
+{
     lateinit var binding : ActivityMain2Binding
     private lateinit var adapter: AdapterClass
     private lateinit var timeTextView: TextView
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         binding = ActivityMain2Binding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -26,48 +28,43 @@ class MainActivity2 : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         adapter = AdapterClass() //ініціалізація списку ланки ГДЗС
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
-        repeat(2) {
-            adapter.addZveno() }
-        binding.btAdd.setOnClickListener {// при тиснені на кнопку додается поле для додаткового газодимозахисника
-            if(adapter.zvenoList.size < 5)  {
-                adapter.addZveno()
+        repeat(2) { adapter.addZveno() }
+            binding.btAdd.setOnClickListener {// при тиснені на кнопку додается поле для додаткового газодимозахисника
+                if(adapter.zvenoList.size < 5)  { adapter.addZveno() }
+                else if (adapter.zvenoList.size>=5) Toast.makeText(this, "Максимум 5 чол. у звені", Toast.LENGTH_SHORT).show()
             }
-            else if (adapter.zvenoList.size>=5) Toast.makeText(this, "Максимум 5 чол. у звені", Toast.LENGTH_SHORT).show()
-        }
-        binding.btRemove.setOnClickListener {// при тиснені на кнопку прибирается 1 поле для газодимозахисника
-            removeLastItem()
-        }
+            binding.btRemove.setOnClickListener {// при тиснені на кнопку прибирается 1 поле для газодимозахисника
+                removeLastItem()
+            }
         binding.recyclerView.addItemDecoration(ItemOffsetDecoration(16))
-        binding.btnSelectTime.setOnClickListener {/*обираємо час входу ланки*/
-            showTimePickerDialog()
-        }
+            binding.btnSelectTime.setOnClickListener {/*обираємо час входу ланки*/
+                showTimePickerDialog()
+            }
     }
-
-
-
 // функція для роботи спінерів
-    fun spinners(){
-    val spinner1: Spinner = binding.spinnerAction
-    val spinnerAdapter1 = ArrayAdapter.createFromResource(this, R.array.action, R.layout.spinner_prompt_item)
-    spinnerAdapter1.setDropDownViewResource(R.layout.spinner_item)
-    spinner1.adapter = spinnerAdapter1
-    spinner1.onItemSelectedListener = this
-    /* додаемо спинер з вибором активности */
-    val spinner2: Spinner = binding.spinnerAction2
-    val spinnerAdapter2 = ArrayAdapter.createFromResource(this, R.array.apparatus, R.layout.spinner_prompt_item)
-    spinnerAdapter2.setDropDownViewResource(R.layout.spinner_item)
-    spinner2.adapter = spinnerAdapter2
-    spinner2.onItemSelectedListener = this
-    /* додаемо спинер з вибором типу апаратів */
-    val spinner3: Spinner = binding.spinnerAction3
-    val spinnerAdapter3 = ArrayAdapter.createFromResource(this, R.array.workload, R.layout.spinner_prompt_item)
-    spinnerAdapter3.setDropDownViewResource(R.layout.spinner_item)
-    spinner3.adapter = spinnerAdapter3
-    spinner3.onItemSelectedListener = this
-    /* додаемо спинер з вибором типу навантаження */
-}
-    fun onClick(view: View) { //функція яка перевіряє введені данні, відправляє їх на слудуючу сторінку, відкриває новий єкран
-
+    fun spinners()
+    {
+        val spinner1: Spinner = binding.spinnerAction
+        val spinnerAdapter1 = ArrayAdapter.createFromResource(this, R.array.action, R.layout.spinner_prompt_item)
+        spinnerAdapter1.setDropDownViewResource(R.layout.spinner_item)
+        spinner1.adapter = spinnerAdapter1
+        spinner1.onItemSelectedListener = this
+        /* додаемо спинер з вибором активности */
+        val spinner2: Spinner = binding.spinnerAction2
+        val spinnerAdapter2 = ArrayAdapter.createFromResource(this, R.array.apparatus, R.layout.spinner_prompt_item)
+        spinnerAdapter2.setDropDownViewResource(R.layout.spinner_item)
+        spinner2.adapter = spinnerAdapter2
+        spinner2.onItemSelectedListener = this
+        /* додаемо спинер з вибором типу апаратів */
+        val spinner3: Spinner = binding.spinnerAction3
+        val spinnerAdapter3 = ArrayAdapter.createFromResource(this, R.array.workload, R.layout.spinner_prompt_item)
+        spinnerAdapter3.setDropDownViewResource(R.layout.spinner_item)
+        spinner3.adapter = spinnerAdapter3
+        spinner3.onItemSelectedListener = this
+        /* додаемо спинер з вибором типу навантаження */
+    }
+    fun onClick(view: View)
+    { //функція яка перевіряє введені данні, відправляє їх на слудуючу сторінку, відкриває новий єкран
         val spinner1: Spinner = binding.spinnerAction
         val spinner2: Spinner = binding.spinnerAction2
         val spinner3: Spinner = binding.spinnerAction3
@@ -76,43 +73,43 @@ class MainActivity2 : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         val selected3 = spinner3.selectedItemPosition
         var sizeList = adapter.zvenoList.size
         var timeBt = binding.btnSelectTime.text.toString()
-
-
-
-
-        when { /*перевіряємо чи вірно введені данні*/
+        when
+        { /*перевіряємо чи вірно введені данні*/
             selected1 == 0 -> showErrorMessage("Оберіть вид роботи!")
             selected2 == 0 -> showErrorMessage("Оберіть вид апаратів")
             selected3 == 0 -> showErrorMessage("Оберіть вид навантаження")
             timeBt == "-Обрати час-" -> showErrorMessage("Оберіть час входу ланки")
             sizeList == 1 -> showErrorMessage("Мінімум 2 чол. в ланці")
             checkAllFieldsFilled() == false -> showErrorMessage("Введіть призвища та тиск")
-
-
             /* якщо все заповнено вірно то при тисненні кнопки запускається екран розрахунків*/
-            else -> {
+            else ->
+            {
                 binding.textAction.visibility = View.GONE
                 val intent = Intent(this, MainActivity3::class.java)
-                val minPressure = adapter.zvenoList.minByOrNull { it.pressure.toInt() }?.pressure?.toInt() ?: 0 /*поиск минимального давления*/
-                val maxPressure = adapter.zvenoList.maxByOrNull { it.pressure.toInt() }?.pressure?.toInt() ?: 0 /*поиск максимального давления*/
-            when {
-                (minPressure<=Constant.minPressureDrager && (selected2 == 1 || selected2 == 3))->showErrorMessage("Мінімальний тиск 270 атм.!")
-                (minPressure<=Constant.minPressureASP && selected2 == 2)-> showErrorMessage("Мінімальний тиск 180 атм.!")
-                (maxPressure>=Constant.maxPressureDrager && (selected2 == 1 || selected2 == 3)) ->showErrorMessage("Максимальний тиск 330 атм.!")
-                (maxPressure>=Constant.maxPressureASP && selected2 == 2) ->showErrorMessage("Максимальний тиск 230 атм.!")
-            else ->{
-                //відправка отриманих данних на слідуючий єкран
-                intent.putExtra("minPressure", minPressure)
-                intent.putExtra("aparat", selected2)
-                intent.putExtra("action", selected3)
-                intent.putExtra("timeAction", timeBt)
-                startActivity(intent)
-                overridePendingTransition(R.anim.fade_in, R.anim.fade_out)}}
+                val minPressure = adapter.zvenoList.minByOrNull { it.pressure.toInt() }?.pressure?.toInt() ?: 0 /*пошук мінімального тиску*/
+                val maxPressure = adapter.zvenoList.maxByOrNull { it.pressure.toInt() }?.pressure?.toInt() ?: 0 /*пошук максимального тиску*/
+                when
+                {
+                    (minPressure<=Constant.minPressureDrager && (selected2 == 1 || selected2 == 3))->showErrorMessage("Мінімальний тиск 270 атм.!")
+                    (minPressure<=Constant.minPressureASP && selected2 == 2)-> showErrorMessage("Мінімальний тиск 180 атм.!")
+                    (maxPressure>=Constant.maxPressureDrager && (selected2 == 1 || selected2 == 3)) ->showErrorMessage("Максимальний тиск 330 атм.!")
+                    (maxPressure>=Constant.maxPressureASP && selected2 == 2) ->showErrorMessage("Максимальний тиск 230 атм.!")
+                    else ->
+                    {
+                     //відправка отриманих данних на слідуючий єкран
+                        intent.putExtra("minPressure", minPressure)
+                        intent.putExtra("aparat", selected2)
+                        intent.putExtra("action", selected3)
+                        intent.putExtra("timeAction", timeBt)
+                        startActivity(intent)
+                        overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+                    }
+                }
             }
-
         }
-            }
-    private fun showErrorMessage(errorMessage: String) {
+    }
+    private fun showErrorMessage(errorMessage: String)
+    {
         binding.textAction.visibility = View.VISIBLE
         binding.textAction.text = errorMessage
         /*функція яка використовується для виводу повідомлень*/
@@ -121,36 +118,36 @@ class MainActivity2 : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     }
     override fun onNothingSelected(parent: AdapterView<*>) {
     }
-    private fun showTimePickerDialog() {
-        // Создаем диалог выбора времени
+    private fun showTimePickerDialog()
+    {// функція для вибору часу
         val calendar = Calendar.getInstance()
         val hourOfDay = calendar.get(Calendar.HOUR_OF_DAY)
         val minute = calendar.get(Calendar.MINUTE)
         val timePickerDialog = TimePickerDialog(this, { _, hour, minute ->
-            // Вызывается, когда пользователь выбирает время
-            var selectedTime = String.format("%02d:%02d", hour, minute)
-            binding.btnSelectTime.text = selectedTime
+        var selectedTime = String.format("%02d:%02d", hour, minute)
+        binding.btnSelectTime.text = selectedTime
         }, hourOfDay, minute, true)
-        // Отображаем диалог выбора времени
         timePickerDialog.show()
     }
-    private fun checkAllFieldsFilled(): Boolean {
-        for (zveno in adapter.zvenoList) {
-            if (zveno.sername.isEmpty() || zveno.pressure.isEmpty()) {
+    private fun checkAllFieldsFilled(): Boolean
+    {
+        for (zveno in adapter.zvenoList)
+        {
+            if (zveno.sername.isEmpty() || zveno.pressure.isEmpty())
+            {
                 return false
             }
         }
         return true
     }
-
-    private fun removeLastItem() { //фунція для видалення поля у звені ГДЗС
+    private fun removeLastItem()
+    { //фунція для видалення поля у звені ГДЗС
         val lastIndex = adapter.zvenoList.lastIndex
-        if (lastIndex > 0) {
+        if (lastIndex > 0)
+        {
             adapter.zvenoList.removeAt(lastIndex)
             adapter.notifyItemRemoved(lastIndex)
         }
     }
-
-
 }
 
--- app/src/main/java/com/zmei/gdzs/MainActivity3.kt
+++ app/src/main/java/com/zmei/gdzs/MainActivity3.kt
@@ -15,15 +15,15 @@ import android.widget.Toast
 import com.zmei.gdzs.databinding.ActivityMain3Binding
 import java.text.SimpleDateFormat
 import java.util.*
-
-
-class MainActivity3 : AppCompatActivity() {
+class MainActivity3 : AppCompatActivity()
+{
     lateinit var binding: ActivityMain3Binding
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         binding = ActivityMain3Binding.inflate(layoutInflater)
         setContentView(binding.root)
-    //отримуємо введені данні
+        //отримуємо введені данні
         val action = intent.getIntExtra("action", 0)
         val minPressure = intent.getIntExtra("minPressure",0)
         val timeAction = intent.getStringExtra("timeAction")
@@ -31,7 +31,7 @@ class MainActivity3 : AppCompatActivity() {
         binding.textTimeEnter.text = timeAction
         var timeWork :Int = ((minPressure-60)/7)
         val newTime = addMinutesToTime(timeAction.toString(), timeWork)
-    //час виходу ланки
+        //час виходу ланки
         binding.textTimeExpected.text = newTime
         binding.textTimeActual.setOnClickListener {
             showTimePickerDialog { selectedTime ->
@@ -39,14 +39,14 @@ class MainActivity3 : AppCompatActivity() {
                 binding.textTimeActual.background = ColorDrawable(Color.TRANSPARENT)
             }
         }
-    //час знаходження осередку пожежі
+        //час знаходження осередку пожежі
         binding.textTimeFire.setOnClickListener {
             showTimePickerDialog { selectedTime ->
                 binding.textTimeFire.text = selectedTime
                 binding.textTimeFire.background = ColorDrawable(Color.TRANSPARENT)
             }
         }
-    //поле для вводу мінімального тиску при знаходжені осередку пожежі
+        //поле для вводу мінімального тиску при знаходжені осередку пожежі
         val editText: EditText = binding.edMinPressure
         editText.setOnClickListener {
             editText.isFocusableInTouchMode = true
@@ -61,75 +61,82 @@ class MainActivity3 : AppCompatActivity() {
                                 val newText = s.toString()
                             }
         })
-//при натисканні кнопки проводиться розрахунок тиску та часу
+        //при натисканні кнопки проводиться розрахунок тиску та часу
         binding.buttonCalcExit.setOnClickListener {
             val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             inputMethodManager.hideSoftInputFromWindow(editText.windowToken, 0)
-        //при виборі типу апаратів з расходом 7 атмосфер
-            if (typeAparat == 1 || typeAparat == 3) {
+            //при виборі типу апаратів з расходом 7 атмосфер
+            if (typeAparat == 1 || typeAparat == 3)
+            {
                 var minPressureNearFire: Int = binding.edMinPressure.text.toString().toIntOrNull() ?: 0
                 if (minPressureNearFire == 0) Toast.makeText(this, "Введіть тиск ", Toast.LENGTH_SHORT).show()
-                else {
+                else
+                {
                     var pressureGo = minPressure - minPressureNearFire
                     binding.textProtection.text =
                         (minPressure / 7).toString() + "хв." //  час захисної дії апарату
-                //рахуеємо тиск який витратили на шлях до осередку пожежі
+                    //рахуеємо тиск який витратили на шлях до осередку пожежі
                     binding.constraintPressure.visibility = View.VISIBLE
                     binding.constraintTime.visibility = View.VISIBLE
                     binding.textPressureGo.text = pressureGo.toString() + "атм."
-                //при виборі середнього навантаження роботи
-                    if (action == 1) {
-                        var pressureExit: Int = pressureGo + 60
-                        var timeWork: Int = (((minPressure - (pressureGo * 2)) - 60) / 7)
-                        binding.textWork.text = timeWork.toString() + "хв." //  час роботи біля осередку пожежі
-                        binding.textPressureExit.text = pressureExit.toString() + "атм." //тиск при котрому потрібно виходити
-                        var minutesExit: Int = (pressureGo / 7) + timeWork
-                        val exitTime = addMinutesToTime(timeAction.toString(), minutesExit)//час на годиннику коли потрібно виходити
-                        binding.textExit.text = exitTime
-                //при виборі важкого навантаження роботи
-                    } else if (action == 2) {
-                        var pressureExit: Int = (2 * pressureGo) + 60
-                        var timeWork: Int = (((minPressure - (pressureGo * 3)) - 60) / 7)
-                        binding.textWork.text = timeWork.toString() + "хв." //  час роботи біля осередку пожежі
-                        binding.textPressureExit.text = pressureExit.toString() + "атм." //тиск при якому потрібно виходити
-                        var minutesExit: Int = (pressureGo / 7) * 2 + timeWork
-                        val exitTime = addMinutesToTime(timeAction.toString(), minutesExit)//час на годиннику коли потрібно виходити
-                        binding.textExit.text = exitTime
-                    }
+                    if (action == 1)
+                        {//при виборі середнього навантаження роботи
+                            var pressureExit: Int = pressureGo + 60
+                            var timeWork: Int = (((minPressure - (pressureGo * 2)) - 60) / 7)
+                            binding.textWork.text = timeWork.toString() + "хв." //  час роботи біля осередку пожежі
+                            binding.textPressureExit.text = pressureExit.toString() + "атм." //тиск при котрому потрібно виходити
+                            var minutesExit: Int = (pressureGo / 7) + timeWork
+                            val exitTime = addMinutesToTime(timeAction.toString(), minutesExit)//час на годиннику коли потрібно виходити
+                            binding.textExit.text = exitTime
+
+                        } else if (action == 2)
+                            {//при виборі важкого навантаження роботи
+                                var pressureExit: Int = (2 * pressureGo) + 60
+                                var timeWork: Int = (((minPressure - (pressureGo * 3)) - 60) / 7)
+                                binding.textWork.text = timeWork.toString() + "хв." //  час роботи біля осередку пожежі
+                                binding.textPressureExit.text = pressureExit.toString() + "атм." //тиск при якому потрібно виходити
+                                var minutesExit: Int = (pressureGo / 7) * 2 + timeWork
+                                val exitTime = addMinutesToTime(timeAction.toString(), minutesExit)//час на годиннику коли потрібно виходити
+                                binding.textExit.text = exitTime
+                            }
                 }
-        //при виборі типу апаратів з расходом 5 атмосфер
-            } else if (typeAparat ==2) {
+            } else if (typeAparat ==2)
+            {//при виборі типу апаратів з расходом 5 атмосфер
                 var minPressureNearFire: Int = binding.edMinPressure.text.toString().toIntOrNull() ?: 0
                 if (minPressureNearFire == 0) Toast.makeText(this, "Введіть тиск ", Toast.LENGTH_SHORT).show()
-                else {
+                else
+                {
                     var pressureGo = minPressure - minPressureNearFire
                     binding.textProtection.text = (minPressure / 5).toString() + "хв." //  час захисної дії апарату
-                //рахуеємо тиск який витратили на шлях до осередку пожежі
+                    //рахуеємо тиск який витратили на шлях до осередку пожежі
                     binding.constraintPressure.visibility = View.VISIBLE
                     binding.constraintTime.visibility = View.VISIBLE
                     binding.textPressureGo.text = pressureGo.toString() + "атм." //тиск використанний на прямування до осередку
-                    if (action == 1) {
-                        var pressureExit: Int = pressureGo + 60
-                        var timeWork: Int = (((minPressure - (pressureGo * 2)) - 60) / 5)
-                        binding.textWork.text = timeWork.toString() + "хв." //  час роботи біля осередку пожежі
-                        binding.textPressureExit.text = pressureExit.toString() + "атм." //тиск при якому потрібно виходити
-                        var minutesExit: Int = (pressureGo / 5) + timeWork
-                        val exitTime = addMinutesToTime(timeAction.toString(), minutesExit)//час на годиннику коли потрібно виходити
-                        binding.textExit.text = exitTime
-                    } else if (action == 2) {
-                        var pressureExit: Int = (2 * pressureGo) + 60
-                        var timeWork: Int = (((minPressure - (pressureGo * 3)) - 60) / 5)
-                        binding.textWork.text = timeWork.toString() + "хв." //  час роботи біля осередку пожежі
-                        binding.textPressureExit.text = pressureExit.toString() + "атм." //тиск при котрому потрібно виходити
-                        var minutesExit: Int = (pressureGo / 5) * 2 + timeWork
-                        val exitTime = addMinutesToTime(timeAction.toString(), minutesExit)//час на годиннику коли потрібно виходити
-                        binding.textExit.text = exitTime
-                    }
+                        if (action == 1)
+                        {
+                            var pressureExit: Int = pressureGo + 60
+                            var timeWork: Int = (((minPressure - (pressureGo * 2)) - 60) / 5)
+                            binding.textWork.text = timeWork.toString() + "хв." //  час роботи біля осередку пожежі
+                            binding.textPressureExit.text = pressureExit.toString() + "атм." //тиск при якому потрібно виходити
+                            var minutesExit: Int = (pressureGo / 5) + timeWork
+                            val exitTime = addMinutesToTime(timeAction.toString(), minutesExit)//час на годиннику коли потрібно виходити
+                            binding.textExit.text = exitTime
+                        } else if (action == 2)
+                            {
+                                var pressureExit: Int = (2 * pressureGo) + 60
+                                var timeWork: Int = (((minPressure - (pressureGo * 3)) - 60) / 5)
+                                binding.textWork.text = timeWork.toString() + "хв." //  час роботи біля осередку пожежі
+                                binding.textPressureExit.text = pressureExit.toString() + "атм." //тиск при котрому потрібно виходити
+                                var minutesExit: Int = (pressureGo / 5) * 2 + timeWork
+                                val exitTime = addMinutesToTime(timeAction.toString(), minutesExit)//час на годиннику коли потрібно виходити
+                                binding.textExit.text = exitTime
+                            }
                 }
             }
-            }
+        }
     }
-    private fun addMinutesToTime(time: String, minutes: Int): String {
+    private fun addMinutesToTime(time: String, minutes: Int): String
+    {
         //Функція для додавання хвилин до поточного часу
         val calendar = Calendar.getInstance()
         val timeFormat = SimpleDateFormat("HH:mm")
@@ -137,7 +144,8 @@ class MainActivity3 : AppCompatActivity() {
         calendar.add(Calendar.MINUTE, minutes)
         return timeFormat.format(calendar.time)
     }
-    private fun showTimePickerDialog(callback: (String) -> Unit) {
+    private fun showTimePickerDialog(callback: (String) -> Unit)
+    {
         val calendar = Calendar.getInstance()
         val hourOfDay = calendar.get(Calendar.HOUR_OF_DAY)
         val minute = calendar.get(Calendar.MINUTE)
