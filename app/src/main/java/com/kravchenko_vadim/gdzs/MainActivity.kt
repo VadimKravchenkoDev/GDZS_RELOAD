ec7693aeaa602c8202f024d754c3820d565fd145
arhizmei <arhizmei@gmail.com>
2023-06-16 15:34:22 +0300

    menu ver.1.7

--- app/src/main/java/com/kravchenko_vadim/gdzs/MainActivity.kt
+++ app/src/main/java/com/kravchenko_vadim/gdzs/MainActivity.kt
@@ -3,20 +3,60 @@ package com.kravchenko_vadim.gdzs
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import com.kravchenko_vadim.gdzs.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //перехід на наступний єкран
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(findViewById<Toolbar>(R.id.toolbar))
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, toolbar, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        binding.navView.setNavigationItemSelectedListener(this)
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         binding.buttonNext.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
-           overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.ac_sign_in -> {
+                Toast.makeText(this, "pressed", Toast.LENGTH_SHORT).show()
+            }
+            R.id.ac_sign_up -> {
+                Toast.makeText(this, "pressed", Toast.LENGTH_SHORT).show()
+            }
+            R.id.ac_sign_out -> {
+                Toast.makeText(this, "pressed", Toast.LENGTH_SHORT).show()
+            }
+            R.id.guidebook_cat2 -> {
+                Toast.makeText(this, "pressed", Toast.LENGTH_SHORT).show()
+            }
+            R.id.settings_cat3 -> {
+                Toast.makeText(this, "pressed", Toast.LENGTH_SHORT).show()
+            }
+            R.id.about_cat4 -> {
+                Toast.makeText(this, "pressed", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
