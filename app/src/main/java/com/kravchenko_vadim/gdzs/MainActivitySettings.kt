36dcef9a165dff2e10eba919421412ae16d324de
arhizmei <arhizmei@gmail.com>
2023-07-02 21:09:15 +0300

    sittings 1.11

--- app/src/main/java/com/kravchenko_vadim/gdzs/MainActivitySettings.kt
+++ app/src/main/java/com/kravchenko_vadim/gdzs/MainActivitySettings.kt
@@ -9,3 +9,5 @@ class MainActivitySettings : AppCompatActivity() {
         setContentView(R.layout.activity_main_settings)
     }
 }
+
+https://www.youtube.com/watch?v=YlsovHtpY_c&list=PLY8G5DMG6TiOBq7OWFPWF2Um3FRB5s2ke&index=62&ab_channel=%D0%9C%D0%BE%D0%B1%D0%B8%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA
