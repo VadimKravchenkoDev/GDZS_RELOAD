14c0d095f1dc418438d410cf4bb1e96e49f1c1cb
Arhizmei <arhizmei@gmail.com>
2023-07-03 20:07:23 +0300

    setting 1.12

--- app/src/main/java/com/kravchenko_vadim/gdzs/MainActivitySettings.kt
+++ app/src/main/java/com/kravchenko_vadim/gdzs/MainActivitySettings.kt
@@ -10,4 +10,3 @@ class MainActivitySettings : AppCompatActivity() {
     }
 }
 
-https://www.youtube.com/watch?v=YlsovHtpY_c&list=PLY8G5DMG6TiOBq7OWFPWF2Um3FRB5s2ke&index=62&ab_channel=%D0%9C%D0%BE%D0%B1%D0%B8%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA
