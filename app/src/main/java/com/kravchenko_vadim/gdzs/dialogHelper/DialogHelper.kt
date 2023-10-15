a62d6190976858d82f8327e54f16f3dfbf02d911
Arhizmei <arhizmei@gmail.com>
2023-10-15 23:58:35 +0300

    add forgot password

--- app/src/main/java/com/kravchenko_vadim/gdzs/dialogHelper/DialogHelper.kt
+++ app/src/main/java/com/kravchenko_vadim/gdzs/dialogHelper/DialogHelper.kt
@@ -2,6 +2,7 @@ package com.kravchenko_vadim.gdzs.dialogHelper
 
 import android.app.AlertDialog
 import android.view.View
+import android.widget.Toast
 import com.kravchenko_vadim.gdzs.MainActivity
 import com.kravchenko_vadim.gdzs.R
 import com.kravchenko_vadim.gdzs.accountHelper.AccountHelper
@@ -20,20 +21,46 @@ class DialogHelper(act: MainActivity) {
         setDialogState(index, binding)
         val dialog = builder.create()
         binding.btSignUpIn.setOnClickListener {
-            dialog.dismiss()
-            if (index == DialogConst.Sign_Up_State){
-                accHelper.signUpWithEmail(binding.edSignEmail.text.toString(), binding.edSignPassword.text.toString())
-            }else{
-                accHelper.signInWithEmail(binding.edSignEmail.text.toString(), binding.edSignPassword.text.toString())
-
-            }
+            setOnClickSignUpIn(index, binding, dialog)
+        }
+        binding.btForgetPass.setOnClickListener {
+            setOnClickResetPassword(binding, dialog)
         }
 
         dialog.show()
     }
 
+    private fun setOnClickResetPassword(binding: SignDialogBinding, dialog: AlertDialog?) {
+        if (binding.edSignEmail.text.isNotEmpty()){
+            act.myFirebaseAuth.sendPasswordResetEmail(binding.edSignEmail.text.toString()).
+            addOnCompleteListener {task-> if (task.isSuccessful){
+                Toast.makeText(act, R.string.reset_email_was_sent, Toast.LENGTH_LONG).show()
+            }
+            }
+            dialog?.dismiss()
+        } else{
+            binding.tvDialogMessage.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setOnClickSignUpIn(index: Int, binding: SignDialogBinding, dialog: AlertDialog?) {
+        dialog?.dismiss()
+        if (index == DialogConst.Sign_Up_State) {
+            accHelper.signUpWithEmail(
+                binding.edSignEmail.text.toString(),
+                binding.edSignPassword.text.toString()
+            )
+        } else {
+            accHelper.signInWithEmail(
+                binding.edSignEmail.text.toString(),
+                binding.edSignPassword.text.toString()
+            )
+
+        }
+    }
+
     private fun setDialogState(index: Int, binding: SignDialogBinding) {
-        if (index == DialogConst.Sign_Up_State){
+        if (index == DialogConst.Sign_Up_State) {
             binding.tvSignTitle.text = act.resources.getString(R.string.ac_sign_up)
             binding.btSignUpIn.text = act.resources.getString(R.string.sign_up_action)
 
